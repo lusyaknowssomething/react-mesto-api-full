696c267580dbe6b13ed6e90baa115f42abd18f0d
Lyudmila Khatanzeyskaya <lyudmila.khatanzeyskaya@yandex.ru>
2022-05-13 23:22:06 +0300

    fix cors

--- backend/app.js
+++ backend/app.js
@@ -57,11 +57,12 @@ app.use('*', Auth, () => {
 });
 
 app.use((req, res, next) => {
+  console.log('origin', allowedCors);
   const { origin } = req.headers; // Сохраняем источник запроса в переменную origin
   // проверяем, что источник запроса есть среди разрешённых
-  console.log(origin);
   if (allowedCors.includes(origin)) {
     res.header('Access-Control-Allow-Origin', origin);
+    res.header('Access-Control-Allow-Credentials', true);
   }
   const { method } = req;
 
