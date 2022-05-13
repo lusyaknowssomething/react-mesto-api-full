c5eea1edc664724c2f6d376dfcaae8abdf9d7b48
Lyudmila Khatanzeyskaya <lyudmila.khatanzeyskaya@yandex.ru>
2022-05-13 16:35:44 +0300

    fix cors

--- backend/app.js
+++ backend/app.js
@@ -11,7 +11,7 @@ const { Auth } = require('./middlewares/auth');
 const NotFoundError = require('./errors/not-found-err');
 const { urlValidation } = require('./middlewares/urlValidation');
 const { requestLogger, errorLogger } = require('./middlewares/logger');
-const { corsValidation } = require('./middlewares/cors');
+const { allowedCors } = require('./middlewares/cors');
 
 const { PORT = 3000 } = process.env;
 const app = express();
@@ -31,6 +31,30 @@ mongoose.connect('mongodb://localhost:27017/mestodb', {
   .catch((error) => console.log(error));
 
 app.use(requestLogger);
+
+app.use((req, res, next) => {
+  const { origin } = req.headers; // Сохраняем источник запроса в переменную origin
+  // проверяем, что источник запроса есть среди разрешённых
+  if (allowedCors.includes(origin)) {
+    res.header('Access-Control-Allow-Origin', origin);
+    res.header('Access-Control-Allow-Headers', true);
+  }
+  const { method } = req;
+
+  // Значение для заголовка Access-Control-Allow-Methods по умолчанию (разрешены все типы запросов)
+  const DEFAULT_ALLOWED_METHODS = 'GET,HEAD,PUT,PATCH,POST,DELETE';
+  const requestHeaders = req.headers['access-control-request-headers'];
+  // Если это предварительный запрос, добавляем нужные заголовки
+  if (method === 'OPTIONS') {
+    // разрешаем кросс-доменные запросы любых типов (по умолчанию)
+    res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
+    // разрешаем кросс-доменные запросы с этими заголовками
+    res.header('Access-Control-Allow-Headers', requestHeaders);
+    // завершаем обработку запроса и возвращаем результат клиенту
+    return res.end();
+  }
+  return next();
+});
 
 app.post('/signin', express.json(), celebrate({
   body: Joi.object().keys({
@@ -54,11 +78,6 @@ app.use('/', Auth, cardsRoutes);
 
 app.use('*', Auth, () => {
   throw new NotFoundError('Запрашиваемый ресурс не найден');
-});
-
-app.use((req, res, next) => {
-  console.log('success');
-  corsValidation(req, res, next);
 });
 
 app.use(errorLogger); // подключаем логгер ошибок
